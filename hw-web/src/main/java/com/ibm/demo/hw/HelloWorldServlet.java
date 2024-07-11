ad6c237c1ed1ee7f1b6b90bfadb2eac2fa78773e
tanuj-k <er.tanujkathuria@gmail.com>
2024-07-11 14:53:07 +0530

    Renamed Hello API path to helloLogs

--- hw-web/src/main/java/com/ibm/demo/hw/HelloWorldServlet.java
+++ hw-web/src/main/java/com/ibm/demo/hw/HelloWorldServlet.java
@@ -10,7 +10,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 
-@WebServlet("/hello")
+@WebServlet("/helloLogs")
 public class HelloWorldServlet extends HttpServlet {
 
 	private static final Logger logger = LogManager.getLogger(HelloWorldServlet.class);
